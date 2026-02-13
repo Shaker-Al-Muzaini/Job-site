15e221dddd7389b0f81dd9f526701b4a39e20f28
Shaker-Al-Muzaini <sshakiralmazini@gmail.com>
2026-02-13 20:23:05 +0200

    pegeg

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
-use ApiPlatform\Metadata\ApiResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'description',
